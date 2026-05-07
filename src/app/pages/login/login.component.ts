af4415cfe81858655f3e3ec93231684922e4d23d
Sujal70 <kalrasujal36@gmail.com>
2026-05-08 01:31:10 +0530

    minor ui fixes

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-login',
@@ -14,16 +15,37 @@ export class LoginComponent {
   rememberMe = false;
   showPassword = false;
   isScrolled = false;
+  isLoading = false;
+  errorMessage = '';
 
   @HostListener('window:scroll')
   onWindowScroll() {
     this.isScrolled = window.scrollY > 50;
   }
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private apiService: ApiService
+  ) {}
 
   onLogin() {
-    console.log('Login attempt:', this.email);
-    this.router.navigate(['/home']);
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.apiService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        if (response.success && response.data) {
+          // Store token
+          localStorage.setItem('token', response.data.token);
+          localStorage.setItem('user', JSON.stringify(response.data.user));
+          this.router.navigate(['/home']);
+        }
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.isLoading = false;
+        this.errorMessage = error.error?.message || 'Login failed. Please try again.';
+      }
+    });
   }
 }
